d17cfabdcde214a289e71a7845ade21b25be41fe
shingaki-max <kokodok@hotmail.co.jp>
2021-04-03 12:08:36 +0900

    ActiveHashでのプルダウンの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :send_date
+
+
+  validates :category_id, presence: true
 end
