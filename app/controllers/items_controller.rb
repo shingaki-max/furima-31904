2901fa1a0feb2abd41f8f2fe6182a33f53a091f3
shingaki-max <kokodok@hotmail.co.jp>
2021-04-12 08:28:24 +0900

    修正　1回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-before_action :authenticate_user!, only: [:new,:create]
+before_action :authenticate_user!, only: [:new,:create, :edit, :update]
+before_action :set_item, only: [:edit, :show, :update]
+before_action :check_user, only: [:edit, :update]
 
   def new
     @item = Item.new
@@ -19,22 +21,12 @@ before_action :authenticate_user!, only: [:new,:create]
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if user_signed_in?
-      if current_user.id != @item.user_id
-        redirect_to root_path
-      end
-    else
-      redirect_to user_session_path
-    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -45,5 +37,15 @@ before_action :authenticate_user!, only: [:new,:create]
 private 
   def item_params
     params.require(:item).permit(:item_name, :category_id, :price, :image, :description, :shipping_charge_id, :state_id, :prefecture_id, :send_date_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_user
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 end
